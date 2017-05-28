b167bdc087cbd7c7f65bec5ccf7a2e16eb2fb4d0
Caleb Smith <caleb.smithnc@gmail.com>
2017-05-28 12:28:21 -0400

    Works through several more problems w/ tequila-django provisioning:
    Everything works until the collectstatic step, which will take some work
    on settings

--- deployment/roles/tequila-django/templates/manage.sh
+++ deployment/roles/tequila-django/templates/manage.sh
@@ -1,3 +1,3 @@
 # Shell script to run a management command
 cd {{ source_dir }}
-{{ venv_dir }}/bin/python {{ source_dir }}/manage.py $@
+{{ venv_dir }}/bin/python {{ source_dir }}/{{ project_name }}/manage.py $@
