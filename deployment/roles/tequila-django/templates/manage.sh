bf5f760c0f58de539c22cb7156e91ae4c874e162
Caleb Smith <caleb.smithnc@gmail.com>
2017-05-28 19:30:56 -0400

    More fixes to gunicorn and integration with shell scripts:
    Needed to put dotenv.sh and .env in the same folder as manage.py

--- deployment/roles/tequila-django/templates/manage.sh
+++ deployment/roles/tequila-django/templates/manage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Shell script to run a management command
-cd {{ source_dir }}
+cd {{ source_dir }}/{{ project_name }}
 ./dotenv.sh && {{ venv_dir }}/bin/python {{ source_dir }}/{{ project_name }}/manage.py $@
