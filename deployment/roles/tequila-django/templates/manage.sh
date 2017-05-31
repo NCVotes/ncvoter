9896e749cd659f00b908ef82fcc336eb11c238a0
Caleb Smith <caleb.smithnc@gmail.com>
2017-05-30 22:04:53 -0400

    Fixes manage.sh script to run commands within dotenv.sh not after

--- deployment/roles/tequila-django/templates/manage.sh
+++ deployment/roles/tequila-django/templates/manage.sh
@@ -2,4 +2,4 @@
 
 # Shell script to run a management command
 cd {{ source_dir }}/{{ project_name }}
-./dotenv.sh && {{ venv_dir }}/bin/python {{ source_dir }}/{{ project_name }}/manage.py $@
+./dotenv.sh {{ venv_dir }}/bin/python {{ source_dir }}/{{ project_name }}/manage.py $@
